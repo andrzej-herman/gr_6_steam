3c1d73c2a3086a1a9bd83f15b0276357824bfadb
Andrzej Herman <andrzejek.herman@gmail.com>
2023-11-17 13:09:55 +0100

    Add project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="h-full flex items-center justify-center pt-60">
+      <h1 className="text-7xl text-red-600">Welcome to Steam !!!</h1>
+    </div>
+  )
+}
+
+
+
+
